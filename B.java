086165ce53c45a0fb43622a0cdd09df2bf768c74
Luiz Felipe <luizfelipesanches2005@gmail.com>
2026-09-22 21:48:20 -0300

    Adiciona as classes A e B com atributos e metodos

---
+++ B.java
@@ -0,0 +1,60 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    // Gets e Sets
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    // Métodos
+    public void MB1() {
+        System.out.println("Método MB1 executado");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2 executado");
+    }
+}
+public class B {
+    private int B1;
+    private float B2;
+
+    // Gets e Sets
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    // Métodos
+    public void MB1() {
+        System.out.println("Método MB1 executado");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2 executado");
+    }
+}
